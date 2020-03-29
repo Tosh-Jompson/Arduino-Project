8c098cb4d8297ed3c6b71a0c7cf5924ed57c2e61
giblets2k <giblets2k@gmail.com>
2020-03-29 22:08:04 +0100

    Re-added marionette ragdoll and walk animation.

---
+++ Arduino-House/Assets/Scripts/Character/SCR_CharacterUpright.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SCR_CharacterUpright : MonoBehaviour
+{
+    new protected Rigidbody rigidbody;
+    public bool keepUpright = true;
+    public float uprightForce = 10f;
+    public float uprightOffset = 1.45f;
+    public float additionalUpwardForce = 10f;
+    public float dampenAngularForce = 0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.maxAngularVelocity = 40; // CANNOT APPLY HIGH ANGULAR FORCES UNLESS THE MAXANGULAR VELOCITY IS INCREASED
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (keepUpright)
+        {
+            // USE TWO FORCES PULLING UP AND DOWN AT THE TOP AND BOTTOM OF THE OBJECT TO PULL IT UPRIGHT
+            rigidbody.AddForceAtPosition(new Vector3(0, (uprightForce + additionalUpwardForce), 0), transform.position + transform.TransformPoint(new Vector3(0, uprightOffset, 0)), ForceMode.Force);
+            rigidbody.AddForceAtPosition(new Vector3(0, -uprightForce, 0), transform.position + transform.TransformPoint(new Vector3(0, -uprightOffset, 0)), ForceMode.Force);
+        }
+    }
+}
