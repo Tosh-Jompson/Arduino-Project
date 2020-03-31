39b19a88ebd337e916a671afc755cb953c2f8855
giblets2k <giblets2k@gmail.com>
2020-03-31 15:16:04 +0100

    Added rotating room with arrow keys

---
+++ Arduino-House/Assets/Scripts/HouseController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HouseController : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKey((KeyCode.UpArrow)))
+        {
+            transform.Rotate(1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey((KeyCode.DownArrow)))
+        {
+            transform.Rotate(-1.0f, 0.0f, 0.0f);
+        }
+
+        if (Input.GetKey((KeyCode.LeftArrow)))
+        {
+            transform.Rotate(0.0f, 0.0f, 1.0f);
+        }
+
+        if (Input.GetKey((KeyCode.RightArrow)))
+        {
+            transform.Rotate(0.0f, 0.0f, -1.0f);
+        }
+    }
+}
